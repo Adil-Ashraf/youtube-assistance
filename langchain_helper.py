6874f4205f3cff512662efe0973720fe01ea40d2
Adil-Ashraf <adil.ashraf@devbox.co>
2024-02-26 13:34:16 +0500

    Added streaming response option

--- langchain_helper.py
+++ langchain_helper.py
@@ -7,7 +7,7 @@ from langchain.chains import LLMChain
 from dotenv import load_dotenv
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
-
+from langchain.callbacks.base import BaseCallbackHandler
 
 load_dotenv()
 
@@ -26,28 +26,28 @@ def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
     return db
 
 
-def get_response_from_query(db, query, k=4):
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text="", display_method='markdown'):
+        self.container = container
+        self.text = initial_text
+        self.display_method = display_method
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        display_function = getattr(self.container, self.display_method, None)
+        if display_function is not None:
+            display_function(self.text)
+        else:
+            raise ValueError(f"Invalid display_method: {self.display_method}")
+
+
+def get_response_from_query(db, query, chat_box, k=4):
     docs = db.similarity_search(query, k=k)
     docs_page_content = " ".join([d.page_content for d in docs])
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k")
+    stream_handler = StreamHandler(chat_box, display_method='write')
 
-    # prompt = PromptTemplate(
-    #     input_variables=["question", "docs"],
-    #     template="""
-    #     You are a helpful assistant that that can answer questions about youtube videos 
-    #     based on the video's transcript.
-
-    #     Answer the following question: {question}
-    #     By searching the following video transcript: {docs}
-
-    #     Only use the factual information from the transcript to answer the question.
-
-    #     If you feel like you don't have enough information to answer the question, say "I don't know".
-
-    #     Your answers should be verbose and detailed.
-    #     """,
-    # )
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", streaming=True, callbacks=[stream_handler])
 
     prompt = PromptTemplate(
         input_variables=["question", "docs"],
@@ -58,12 +58,11 @@ def get_response_from_query(db, query, k=4):
         Utilize the video transcript provided:
 
         Transcript: {docs}
-        Respond with concise information, limiting your response to 80 words. If the transcript lacks sufficient detail for a definitive answer, state "I don't know." Your response should be clear, factual, and directly derived from the provided transcript.
+        Respond with bullet-pointed, concise information, limiting your response to 120 words. If the transcript lacks sufficient detail for a definitive answer, state "I don't know." Your response should be clear, factual, and directly derived from the provided transcript.
         """,
     )
     chain = LLMChain(llm=llm, prompt=prompt)
 
     response = chain.run(question=query, docs=docs_page_content)
-    # response = response.replace("\n", "")
-    return response, docs
-# print(create_vector_db_from_youtube_url("https://www.youtube.com/watch?v=-Osca2Zax4Y"))
+    response = response.replace("\n", "")
+    return response
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import langchain_helper as lch
-import textwrap
 
-st.title("YouTube Assistant")
+st.title("DevBox YouTube Assistant")
 
 with st.sidebar:
     with st.form(key='my_form'):
         youtube_url = st.sidebar.text_area(
-            label="What is the YouTube video URL?",
-            max_chars=50
+            label="Enter the YouTube video URL here!",
+            max_chars=100
             )
         query = st.sidebar.text_area(
             label="Ask me about the video?",
-            max_chars=50,
+            max_chars=100,
             key="query"
             )
 
@@ -20,6 +19,6 @@ with st.sidebar:
 
 if query and youtube_url:
     db = lch.create_vector_db_from_youtube_url(youtube_url)
-    response, docs = lch.get_response_from_query(db, query)
     st.subheader("Answer:")
-    st.text(textwrap.fill(response, width=85))
+    chat_box = st.empty()
+    lch.get_response_from_query(db, query,chat_box)
